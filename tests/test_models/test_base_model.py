f61d7984624956bd0b90538098c396b802484155
Ante-237 <akumawahanye@gmail.com>
2023-02-16 22:38:39 +0200

    change of test to fix it

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -90,11 +90,10 @@ class test_basemodel(unittest.TestCase):
         new = self.value()
         self.assertEqual(type(new.created_at), datetime.datetime)
     
-    @unittest.skip()
     def test_updated_at(self):
         """ """
         new = self.value()
         self.assertEqual(type(new.updated_at), datetime.datetime)
         n = new.to_dict()
         new = BaseModel(**n)
-        self.assertFalse(new.created_at == new.updated_at)
+        self.assertTrue(new.created_at == new.updated_at)
