82877e7b1391e55e16786be9c1db8b1ef2e07940
Ante-237 <akumawahanye@gmail.com>
2023-02-20 17:02:08 +0200

    more changes to console

--- models/review.py
+++ models/review.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class Review(BaseModel):
     """ Review classto store review information """
     place_id = ""
     user_id = ""
     text = ""
+    
+    __tablename__ = "reviews"
+
+    place_id = Column(String(60), nullable=False)
+    user_id = Column(String(60), nullable=False)
+    text = Column(String(1024), nullable=False)
+    users = relationship("User", backref="user", cascade="delete")
