679112cfb6ac69a69bfe50b61fc2215054c255ab
Ante-237 <akumawahanye@gmail.com>
2023-02-24 18:35:21 +0200

    changes to base model

--- models/base_model.py
+++ models/base_model.py
@@ -63,9 +63,8 @@ class BaseModel:
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        for key in self.__dict__.keys():
-            if key == "_sa_instance_state":
-                del (dictionary[key])
+        if '_sa_instance_state' in dictionary:
+            del dictionary['_sa_instance_state']
 
         dictionary.update({'__class__': self.__class__.__name__})
         dictionary['created_at'] = self.created_at.isoformat()
