f9bf2a6a9a5f4ce674cf1c2ad3fc1a6fada38ae6
Ante-237 <akumawahanye@gmail.com>
2023-02-24 20:35:27 +0200

    changes to setup of mysql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,8 +1,8 @@
--- create database hbnb_dev_db
+-- create database hbnb_dev_db, test database creation
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- add aa new user hbnb_dev with password 'hbnb_dev_pwd'
+-- add aa new user hbnb_dev with password 'hbnb_dev_pwd', user and password
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
--- give privileges to hbnb_dev
+-- give privileges to hbnb_dev, acess rights
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+-- FLUSH PRIVILEGES;
