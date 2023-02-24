c6efa75365e009715a2ab4b284712fe1f61513d9
Ante-237 <akumawahanye@gmail.com>
2023-02-24 20:48:50 +0200

    few changes to console.py

--- console.py
+++ console.py
@@ -153,67 +153,65 @@ class HBNBCommand(cmd.Cmd):
 
         print(new_instance.id)
         new_instance.save()
-"""
-    def do_create(self, args):
-        #  Create an object of any class
-        # print(args)
-        # getting the arguments in place
-        items = args.split(" ")
-        # print(items)
-        # get first argument
-        class_args = str(items[0])
-        param_list = []
-        dict_list = {}
-
-        for temp_one in items:
-            if '=' in temp_one:
-                param_list.append(temp_one)
-
-        # divid temp list
-        temp_one_list = []
-        for temp_one in param_list:
-
-            holder_list = temp_one.split('=')
-            # check for string 
-            if '\"' in temp_one:
-                holder_list[1] = str(holder_list[1].replace('\"', ''))
-                if '_' in holder_list[1]:
-                    holder_list[1].replace('_','')
-
-            elif '.' in holder_list[1]:
-                # check for float
-                holder_list[1] = float(holder_list[1])
-                # check for int
-            elif '.' not in temp_one \
-                    and '_' not in holder_list[1] \
-                    and '\"' not in holder_list[1]:
-                holder_list[1] = int(holder_list[1])
-
-            temp_one_list.append(holder_list)
-
-        # print(temp_one_list)
-
-
-        if not args:
-            print("** class name missing **")
-            return
-        elif class_args not in HBNBCommand.classes:
-            print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[class_args]()
-
-        # set the attirbutes to the instance
-        if len(temp_one_list) > 0:
-            for temp_one in temp_one_list:
-                if hasattr(new_instance, temp_one[0]):
-                    setattr(new_instance, temp_one[0], temp_one[1])
-
-        # storage.save()
-        print(new_instance.id)
-        new_instance.save()
-"""
-
-
+    """
+        def do_create(self, args):
+            #  Create an object of any class
+            # print(args)
+            # getting the arguments in place
+            items = args.split(" ")
+            # print(items)
+            # get first argument
+            class_args = str(items[0])
+            param_list = []
+            dict_list = {}
+    
+            for temp_one in items:
+                if '=' in temp_one:
+                    param_list.append(temp_one)
+    
+            # divid temp list
+            temp_one_list = []
+            for temp_one in param_list:
+    
+                holder_list = temp_one.split('=')
+                # check for string 
+                if '\"' in temp_one:
+                    holder_list[1] = str(holder_list[1].replace('\"', ''))
+                    if '_' in holder_list[1]:
+                        holder_list[1].replace('_','')
+    
+                elif '.' in holder_list[1]:
+                    # check for float
+                    holder_list[1] = float(holder_list[1])
+                    # check for int
+                elif '.' not in temp_one \
+                        and '_' not in holder_list[1] \
+                        and '\"' not in holder_list[1]:
+                    holder_list[1] = int(holder_list[1])
+    
+                temp_one_list.append(holder_list)
+    
+            # print(temp_one_list)
+    
+    
+            if not args:
+                print("** class name missing **")
+                return
+            elif class_args not in HBNBCommand.classes:
+                print("** class doesn't exist **")
+                return
+            new_instance = HBNBCommand.classes[class_args]()
+    
+            # set the attirbutes to the instance
+            if len(temp_one_list) > 0:
+                for temp_one in temp_one_list:
+                    if hasattr(new_instance, temp_one[0]):
+                        setattr(new_instance, temp_one[0], temp_one[1])
+    
+            # storage.save()
+            print(new_instance.id)
+            new_instance.save()
+    """
     def help_create(self):
         """ Help information for the create method """
         print("Creates a class of any type")
